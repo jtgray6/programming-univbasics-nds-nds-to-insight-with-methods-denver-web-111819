80f4bdf7d208c191c0075f7f9d10a78c7b3e22a8
Jeremy Gray <jeremygray@utexas.edu>
2019-11-17 00:18:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,15 +29,15 @@ end
 def gross_for_director(director_data)
   # pp director_data
   # binding.pry
-  # gross=0
-  # row=0
-  # while row<director_data.length do
-  #   column=0
-  #   while column<director_data[row][:movies].length do
-  #     gross+=director_data[row][:movies][column][:worldwide_gross]
-  #     column+=1
-  #   end
-  #   row+=1
-  # end
-  # gross
+  gross=0
+  row=0
+  while row<director_data.length do
+    column=0
+    while column<director_data[row][:movies].length do
+      gross+=director_data[row][:movies][column][:worldwide_gross]
+      column+=1
+    end
+    row+=1
+  end
+  gross
 end
